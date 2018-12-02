650202d02b3a0a5b8a8ef9336ebb8f9bb513bdde
Francisco Javier Hidalgo Frias <friasjavi45@gmail.com>
2018-12-02 20:02:01 +0100

    Finalizado MaterialDesign09

--- app/src/main/java/com/iesvirgendelcarmen/dam/materialdesign09/Material09.java
+++ app/src/main/java/com/iesvirgendelcarmen/dam/materialdesign09/Material09.java
@@ -6,18 +6,29 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 public class Material09 extends AppCompatActivity {
 
+    private static final int DURACION = 250;
+
     Toolbar toolbar;
     Context context;
+    boolean giro;
+    ImageView icono;
+    LinearLayout linear3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_material09);
         toolbar = findViewById(R.id.toolbar);
+        linear3 = findViewById(R.id.linear3);
         context = getBaseContext();
+        giro = false;
 
         toolbar.inflateMenu(R.menu.menu);
         toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
@@ -37,8 +48,31 @@ public class Material09 extends AppCompatActivity {
                 }
             }
         });
+
+        icono = findViewById(R.id.imagen2);
+
     }
 
     public void tocado(View view) {
+        if(giro){
+            icono.setImageResource(R.mipmap.menos);
+            rotate(-180.0f);
+            Dinamizador.contrae(linear3, DURACION);
+            giro = false;
+        } else {
+            icono.setImageResource(R.mipmap.mas);
+            rotate(180.0f);
+            Dinamizador.expande(linear3, DURACION);
+            giro = true;
+        }
+
+    }
+
+    private void rotate(float angulo){
+        Animation animation = new RotateAnimation(0.0f, angulo, Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f);
+        animation.setFillAfter(true);
+        animation.setDuration(DURACION);
+        icono.startAnimation(animation);
     }
 }
